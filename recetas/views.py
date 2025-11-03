f798055c628071b9daa002ced86ee19f6b845f45
MarceDokken <mjimenez.dev@gmail.com>
2025-11-02 21:14:11 -0300

    4to Commit: Se implementa sistema de autenticación de usuarios

--- recetas/views.py
+++ recetas/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Receta
 
-# Create your views here.
+def lista_recetas(request):
+    """Vista para listar todas las recetas"""
+    recetas = Receta.objects.all().order_by('-fecha_creacion')
+    return render(request, 'recetas/lista_recetas.html', {'recetas': recetas})
+
+def detalle_receta(request, receta_id):
+    """Vista para ver el detalle de una receta"""
+    receta = Receta.objects.get(id=receta_id)
+    return render(request, 'recetas/detalle_receta.html', {'receta': receta})
+
+def home(request):
+    """Página de inicio"""
+    return render(request, 'recetas/home.html')
